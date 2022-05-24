5c3b55c8a72523e4f8d2d92550a7f522108be746
shiva moeini <shivamoeini2000@gmail.cpm>
2022-05-24 23:00:37 +0430

    cmit2

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const express=require('express');
+const router=require('./routes/routes')
+const app=express();
+
+app.use('/api/tasks',router)
+app.listen('8000',err=>{
+    if(err) console.log(err)
+    console.log('server is started at PORT number:8000');
+})
